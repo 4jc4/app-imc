a609d2ceaabd1a1e6500611c49262d0eab1a4efd
Cardoso <ajca.dev@gmail.com>
2026-01-11 10:02:28 -0400

    fix message error

--- src/schemas/bmiSchema.ts
+++ src/schemas/bmiSchema.ts
@@ -1,8 +1,9 @@
 import { z } from "zod";
 
 export const bmiSchema = z.object({
-  weight: z.number().min(2, "Peso muito baixo").max(500, "Peso inválido"),
-  height: z.number().min(0.5, "Altura muito baixa").max(3, "Altura inválida"),
+  weight: z.number({ message: "Peso é obrigatório" }).min(2, "Muito baixo"),
+
+  height: z.number("Altura é obrigatória").min(0.5, "Muito baixa"),
 });
 
 export type BmiFormData = z.infer<typeof bmiSchema>;
